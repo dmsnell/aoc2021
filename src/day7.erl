a58769f55f1f30f03d099435d18bba594d9b1214
Dennis Snell <dennis.snell@automattic.com>
2021-12-06 23:50:39 -0700

    Better optimization for day 7

--- src/day7.erl
+++ src/day7.erl
@@ -8,23 +8,11 @@
 input_type() -> numbers.
 
 p1(Input) ->
-    {Min, Max} = extrema(Input),
     lists:min([
         lists:sum([abs(Crab - N) || Crab <- Input])
         ||
-        N <- lists:seq(Min, Max)
+        N <- unique(Input)
     ]).
-
-
-fuel_until(Xs, Max, Cost) ->
-    fuel_until(Xs, Max, Cost, 0).
-
-fuel_until(_Xs, Max, _Cost, Fuel) when Fuel > Max ->
-    Max + 1;
-fuel_until([], Max, _Cost, Fuel) ->
-    min(Max, Fuel);
-fuel_Until([X | Xs], Max, Cost, Fuel) ->
-    fuel_until(Xs, Max, Cost, Fuel + Cost(X)).
 
 
 -ifdef(EUNIT).
@@ -35,11 +23,10 @@ p1_test() ->
 -endif.
 
 p2(Input) ->
-    {Min, Max} = extrema(Input),
     lists:min([
         lists:sum([fuel(C, N) || C <- Input])
         ||
-        N <- lists:seq(Min, Max)
+        N <- unique(Input)
     ]).
 
 
@@ -58,14 +45,5 @@ p2_test() ->
 
 example() -> [16,1,2,0,4,2,7,1,2,14].
 
-
-extrema([First | List]) ->
-    extrema(List, First, First).
-
-extrema([], Max, Min) -> {Min, Max};
-extrema([A | List], Max, Min) when A > Max ->
-    extrema(List, A, Min);
-extrema([A | List], Max, Min) when A < Min ->
-    extrema(List, Max, A);
-extrema([_ | List], Max, Min) ->
-    extrema(List, Max, Min).
+unique(L) ->
+    sets:to_list(sets:from_list(L)).
