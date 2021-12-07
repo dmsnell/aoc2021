3cfac6c9e0edd3d57f17f200796ea27860dd7f03
Dennis Snell <dennis.snell@automattic.com>
2021-12-06 23:27:02 -0700

    Small updates to day 7

--- src/day7.erl
+++ src/day7.erl
@@ -8,13 +8,23 @@
 input_type() -> numbers.
 
 p1(Input) ->
-    Min = lists:min(Input),
-    Max = lists:max(Input),
+    {Min, Max} = extrema(Input),
     lists:min([
         lists:sum([abs(Crab - N) || Crab <- Input])
         ||
         N <- lists:seq(Min, Max)
     ]).
+
+
+fuel_until(Xs, Max, Cost) ->
+    fuel_until(Xs, Max, Cost, 0).
+
+fuel_until(_Xs, Max, _Cost, Fuel) when Fuel > Max ->
+    Max + 1;
+fuel_until([], Max, _Cost, Fuel) ->
+    min(Max, Fuel);
+fuel_Until([X | Xs], Max, Cost, Fuel) ->
+    fuel_until(Xs, Max, Cost, Fuel + Cost(X)).
 
 
 -ifdef(EUNIT).
@@ -25,8 +35,7 @@ p1_test() ->
 -endif.
 
 p2(Input) ->
-    Min = lists:min(Input),
-    Max = lists:max(Input),
+    {Min, Max} = extrema(Input),
     lists:min([
         lists:sum([fuel(C, N) || C <- Input])
         ||
@@ -35,15 +44,28 @@ p2(Input) ->
 
 
 fuel(A, B) ->
-    (1 + abs(B - A)) / 2 * abs(B - A).
+    Delta = abs(B - A),
+    round((1 + Delta) / 2 * Delta).
 
 -ifdef(EUNIT).
 
 p2_test() ->
-    ?assertEqual(168, p1(example())).
+    ?assertEqual(168, p2(example())).
 
 -endif.
 
 %%% Helpers
 
 example() -> [16,1,2,0,4,2,7,1,2,14].
+
+
+extrema([First | List]) ->
+    extrema(List, First, First).
+
+extrema([], Max, Min) -> {Min, Max};
+extrema([A | List], Max, Min) when A > Max ->
+    extrema(List, A, Min);
+extrema([A | List], Max, Min) when A < Min ->
+    extrema(List, Max, A);
+extrema([_ | List], Max, Min) ->
+    extrema(List, Max, Min).
